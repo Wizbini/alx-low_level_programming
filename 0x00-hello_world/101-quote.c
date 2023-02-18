c538d673bd2bed9a5704454fcd2fc15d4e46ac33
biniyam yilma <binaw85@gmail.com>
2023-02-18 12:49:40 -0800

    UNIX is basically

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that prints exactly with proper grammar,.
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\"
+- Dora Korpar, 2015 - 10 - 19\n", 59);
+	return (1);
+}
