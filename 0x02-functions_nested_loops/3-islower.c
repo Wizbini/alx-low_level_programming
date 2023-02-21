8d4d46f0db24059eb3da88b8c768cbdbd1560dd6
biniyam yilma <binaw85@gmail.com>
2023-02-21 11:37:54 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - checks for lower case.
+ *
+ * Return: 1 for lowercase or 0 for else
+ */
+
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
