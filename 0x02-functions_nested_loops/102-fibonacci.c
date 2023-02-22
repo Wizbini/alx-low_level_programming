3727b712f1d4a4ad2093da9cd67a3b5df477a030
biniyam yilma <binaw85@gmail.com>
2023-02-21 17:25:05 -0800

    task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,22 +9,22 @@ int main(void)
 
 {
 	int i = 0;
-	int j = i, k = 2;
+	long j = i, k = 2;
 
 	while (i < 50)
 	{
 		if (i == 0)
-			printf("%d", j);
+			printf("%ld", j);
 		else if (i == 1)
-			printf(", %d", k);
+			printf(", %ld", k);
 		else
 		{
 			k += j;
 			j = k - j;
-			printf(", %d", k);
+			printf(", %ld", k);
 		}
 		++i;
 	}
-	printf("\n');
+	printf("\n");
 	return (0);
 }
